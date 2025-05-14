c7afc2b83c9acf4be8e301aad8260a0bd072a8ab
MtnDewIt <107611247+MtnDewIt@users.noreply.github.com>
2025-05-14 16:58:13 +1000

    Migrate to .NET Core

--- JsonToMoppCLR/JsonMoppNet.cpp
+++ JsonToMoppCLR/JsonMoppNet.cpp
@@ -4,23 +4,26 @@
 
 inline MemoryStream ^ JsonMoppNet::JsonToMopp::CreateMopp(String ^ json)
 {
-MemoryStream^ output = gcnew MemoryStream();
+    MemoryStream^ output = gcnew MemoryStream();
+    
+    msclr::interop::marshal_context context;
 
-msclr::interop::marshal_context context;
-std::string standardString = context.marshal_as<std::string>(json);
+    std::string standardString = context.marshal_as<std::string>(json);
+    
+    HavokShapeListMoppCompiler compiler;
 
-HavokShapeListMoppCompiler compiler;
-compiler.Init();
-std::vector<char> mopp = compiler.MoppFromJson(standardString);
-compiler.Quit();
+    compiler.Init();
+    std::vector<char> mopp = compiler.MoppFromJson(standardString);
+    compiler.Quit();
+    
+    array<unsigned char>^ _buffer = gcnew array<unsigned char>(mopp.size());
 
-array<unsigned char>^ _buffer = gcnew array<unsigned char>(mopp.size());
-for (int i = 0, len = mopp.size(); i < len; ++i)
-{
-_buffer[i] = mopp[i];
-}
-
-output->Write(_buffer, 0, (int)mopp.size());
-output->Position = 0;
-return output;
+    for (int i = 0, len = mopp.size(); i < len; ++i)
+    {
+        _buffer[i] = mopp[i];
+    }
+    
+    output->Write(_buffer, 0, (int)mopp.size());
+    output->Position = 0;
+    return output;
 }
